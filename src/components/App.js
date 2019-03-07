ae1d6c3d75e152e126dd71f0b7a9e87cc522c222
Virgil Martinez <virgilmartinez@gmail.com>
2019-03-07 11:57:36 -0600

    Initializing project

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Go to Page 2</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me!</button>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
